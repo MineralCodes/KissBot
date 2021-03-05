2e70f2df20a48f3e6b49907e1762da1e0c6da49a
Micah Gilbertson <micah.gilbertson@gmail.com>
2021-03-05 02:50:53 +0000

    now sends image of a cat with the !kiss? command

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import discord;
-import os;
-TOKEN = os.getenv("TOKEN");
+import discord
+import requests
+import os
 
-client = discord.Client();
+_DISCORD_TOKEN = os.getenv("TOKEN")
+_CATS_API_KEY = os.getenv("CAT_API")
+cats_url = "https://api.thecatapi.com/v1/images/search?limit=1&size=small&mime_types=jpg,png"
+
+def get_image(user):
+    response = requests.get(f"{cats_url}&sub_id={user}", headers={"x-api-key": "e35526e7-d4cb-49ee-a53d-774265c47598"})
+    return response.json()
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"We have loged in as {client.user}");
+    print(f"We have loged in as {client.user}")
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return;
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+        return
 
-client.run(TOKEN);
+    if message.content.startswith("!Kiss?") or message.content.startswith("!kiss?"):
+        image_info = get_image(message.author)
+        await message.channel.send(image_info[0]["url"])
+
+client.run(_DISCORD_TOKEN)
