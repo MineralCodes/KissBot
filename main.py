a27d2656c11641f124002f839131c79455d5b258
Micah Gilbertson <micah.gilbertson@gmail.com>
2021-03-23 05:46:27 +0000

    added some new commands. Now will respond to bonk, banish, and help commands

--- main.py
+++ main.py
@@ -21,10 +21,35 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("!Kiss?") or message.content.startswith("!kiss?"):
+    content = message.content
+    if content.startswith("!Kiss?") or content.startswith("!kiss?"):
         image_info = get_image(message.author)
         await message.channel.send(image_info[0]["url"])
-    elif message.content.startswith("!Bippy?") or message.content.startswith("!bippy?"):
+    elif content.startswith("!bippy?"):
         await message.channel.send("https://www.youtube.com/watch?v=jamjHQ_V_dQ")
+    elif content.startswith("!bonk?"):
+        mentions = message.mentions
+        if len(mentions) > 0:
+            channel = message.guild.afk_channel
+            user = mentions[0]
+            await user.move_to(channel, reason="For the memes")
+
+        elif len(mentions) > 1:
+            await message.channel.send("you may currently only bonk one user at a time")
+        else:
+            await message.channel.send("you must mention the user you wish to bonk")
+    elif content.startswith("!banish?"):
+        if len(mentions) > 0:
+            user = mentions[0]
+            await user.move_to(None, reason="For the memes")
+
+        elif len(mentions) > 1:
+            await message.channel.send("you may currently only banish one user at a time")
+        else:
+            await message.channel.send("you must mention the user you wish to banish")
+
+    elif content.startswith("!help?"):
+        help_info = "'!Kiss?' or '!kiss' to send a random cat image\n'!bippy?' to send the bippy video\n'!bonk? @user_mention' to send user to horny jail'\n'!banish? @user_mention' to disconnnect a user"
+        await message.channel.send(help_info)
 
 client.run(_DISCORD_TOKEN)
